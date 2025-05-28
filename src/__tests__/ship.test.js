22dcf61e893c3522380186a06072137184d37a29
Visioness <aygun-08-zurnaci@hotmail.com>
2025-05-28 17:43:33 +0300

    Implement tests for Ship class

---
+++ src/__tests__/ship.test.js
@@ -0,0 +1,77 @@
+const Ship = require('./../modules/ship');
+
+describe('Ship', () => {
+  test('should be initialized with a name', () => {
+    const ship = new Ship('Carrier');
+    expect(ship.length).toBe(5);
+
+    const ship2 = new Ship('Battleship');
+    expect(ship2.length).toBe(4);
+
+    const ship3 = new Ship('Destroyer');
+    expect(ship3.length).toBe(3);
+
+    const ship4 = new Ship('Submarine');
+    expect(ship4.length).toBe(3);
+
+    const ship5 = new Ship('Patrol Boat');
+    expect(ship5.length).toBe(2);
+  });
+
+  test('should be initialized with a hit count of 0', () => {
+    const ship = new Ship('Carrier');
+    expect(ship.hitCount).toBe(0);
+
+    const ship2 = new Ship('Battleship');
+    expect(ship2.hitCount).toBe(0);
+
+    const ship3 = new Ship('Destroyer');
+    expect(ship3.hitCount).toBe(0);
+
+    const ship4 = new Ship('Submarine');
+    expect(ship4.hitCount).toBe(0);
+
+    const ship5 = new Ship('Patrol Boat');
+    expect(ship5.hitCount).toBe(0);
+  });
+
+  test('should be able to be hit', () => {
+    const ship = new Ship('Carrier');
+    ship.hit();
+    expect(ship.hitCount).toBe(1);
+    ship.hit();
+    expect(ship.hitCount).toBe(2);
+    ship.hit();
+    expect(ship.hitCount).toBe(3);
+    ship.hit();
+    expect(ship.hitCount).toBe(4);
+    ship.hit();
+    expect(ship.hitCount).toBe(5);
+    ship.hit();
+    expect(ship.hitCount).toBe(5);
+
+    const ship2 = new Ship('Patrol Boat');
+    ship2.hit();
+    expect(ship2.hitCount).toBe(1);
+    ship2.hit();
+    expect(ship2.hitCount).toBe(2);
+    ship2.hit();
+    expect(ship2.hitCount).toBe(2);
+  });
+
+  test('should be able to be sunk', () => {
+    const ship = new Ship('Submarine');
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+
+    const ship2 = new Ship('Patrol Boat');
+    ship2.hit();
+    expect(ship2.isSunk()).toBe(false);
+    ship2.hit();
+    expect(ship2.isSunk()).toBe(true);
+  });
+});
